d0cf4389968fe417301d479299711bebfabc7e65
Blair Peterson and Polina Nenchev <polina.mihova@yahoo.com>
2016-02-15 11:57:41 -0800

    Add user interface logic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,26 +7,30 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
-  //   String layout = "templates/layout.vtl";
-  //
-  //   get("/", (request, response) -> {
-  //     Map<String, Object> model = new HashMap<String, Object>();
-  //     model.put("template", "templates/main.vtl");
-  //     return new ModelAndView(model, layout);
-  //   }, new VelocityTemplateEngine());
-  //
-  //   get("/triangle", (request, response) -> {
-  //     Map<String, Object> model = new HashMap<String, Object>();
-  //
-  //     int sideA = Integer.parseInt(request.queryParams("sideA"));
-  //     int sideB = Integer.parseInt(request.queryParams("sideB"));
-  //     int sideC = Integer.parseInt(request.queryParams("sideC"));
-  //
-  //     Triangle myTriangle = new Triangle(sideA, sideB, sideC);
-  //     model.put("myTriangle", myTriangle);
-  //
-  //     model.put("template", "templates/triangle.vtl");
-  //     return new ModelAndView(model, layout);
-  //   }, new VelocityTemplateEngine());
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/main.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/parcel", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+
+      int height = Integer.parseInt(request.queryParams("height"));
+      int width = Integer.parseInt(request.queryParams("width"));
+      int length = Integer.parseInt(request.queryParams("length"));
+      int weight = Integer.parseInt(request.queryParams("weight"));
+      int distance = Integer.parseInt(request.queryParams("distance"));
+
+    }
+
+      Parcels myParcels = new Parcels(height, width, length, weight, distance);
+      model.put("myParcels", myParcels);
+
+      model.put("template", "templates/parcel.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
    }
 }
